f355d10b56fc4175fa0bad5e9045042904f8dcee
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-11-26 18:22:58 -0300

    Adicionar a solução o exercício Multiplo.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("----Multíplo----");
+int n1, n2;
+Console.WriteLine();
+
+Console.Write("Digete um número...:"); 
+n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Analisar se é multiplo de....:");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % n2 == 0)
+{
+    Console.WriteLine($"{n1} é multiplo de {n2}");
+}
+else
+{
+    Console.WriteLine($"{n1} não é multiplo de {n2}");
+}
+
+
